41512096f2df4b2f11a63344e02850fea902d46b
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-29 21:47:30 +0530

    finalise project - change heading colour

--- server.py
+++ server.py
@@ -9,19 +9,19 @@ print(CHOSEN_NUMBER)
 
 @app.route("/")
 def index():
-    return "<h1>Guess a number between 0 and 9!</h1>" \
+    return f"<h1>Guess a number between 0 and 9!</h1>" \
            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
 
 
 @app.route("/<int:guess>")
 def guess_number(guess):
     if guess < CHOSEN_NUMBER:
-        return f"<h1 color='red'>{guess} is too low</h1>" \
+        return f"<h1 style='color:red'>{guess} is too low</h1>" \
                "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
     elif guess > CHOSEN_NUMBER:
-        return f"<h1 color='red'>{guess} is too high</h1>" \
+        return f"<h1 style='color:blue'>{guess} is too high</h1>" \
                "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
-    return f"<h1 text-color='green'>You found me!</h1>" \
+    return f"<h1 style='color:green'>You found me!</h1>" \
            f"<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
 
 
